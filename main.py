65ceb4033f5069b9d8c9063b7ace257b0f0c6c6a
Dragos-Andrei Ilies <dragos-andrei.ilies@mail.bcu.ac.uk>
2018-09-25 15:22:43 +0100

    Added fixation for all gambles

--- main.py
+++ main.py
@@ -18,17 +18,19 @@ et = et.loc[:,["TimeStamp","Event","GazePointX","GazePointY"]]
 def get_gambles():
     trials = [] # Here we store the tuples
     ph = 0 #A place holder for start-gamble index
-    op = pd.DataFrame()
+    op = pd.DataFrame() 
+    #Loop though the indexes and take only the indexes the start and end gamble 
     for i in et.index:
-        val = et.at[i,"Event"]
+        val = et.at[i,"Event"] # The value at the current cell
         if "gamble" in str(val):       
-            if ph != 0:   
-                trials.append((ph,i))
-                ph = 0 
-            else:
+            if ph != 0:   # if ph !=0 then ph is start-gamble
+                trials.append((ph,i)) #Append the ph and the current index
+                ph = 0 #Reset
+            else: # If ph is 0 then take the index of start-gamble
                 ph = i 
                 
-    for f in trials:
+    #loop through the gambles indexes pairs and add them to a data            
+    for f in trials: 
         first__gamble_index = f[0]+1 # take the first tuple value - start-gamble
         last_gamble_index = (f[1]) # take the second tuple value - end gamble
         op = op.append(et.iloc[first__gamble_index:last_gamble_index]) 
@@ -40,24 +42,29 @@ def get_gambles():
     
 
 
-
+def get_fixations():
+    opListX = op["GazePointX"].tolist()
+    opListY = op["GazePointY"].tolist()
+    opListT = op["TimeStamp"].tolist()
+    
+    
+    results = fixation_detection(opListX, opListY, opListT, missing=0.0, maxdist=25, mindur=20)
+    fixations = results[1]
+    return fixations
+    
 
  
 start = time.time()           
 
 op = get_gambles()
+fixations = get_fixations()
+
 
 
 end = time.time()
 print(end - start)
 
-opListX = op["GazePointX"].tolist()
-opListY = op["GazePointY"].tolist()
-opListT = op["TimeStamp"].tolist()
 
-
-results = fixation_detection(opListX, opListY, opListT, missing=0.0, maxdist=25, mindur=20)
-fixations = results[1]
 
 
 
